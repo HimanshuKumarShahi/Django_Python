99da8d53ff9690986918194f1949d69712bd4055
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-01-21 00:10:00 +0530

    configuring tailwind solve error.

--- chaiaurDjango/chaiaurDjango/settings.py
+++ chaiaurDjango/chaiaurDjango/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chai',
+    "tailwind",
+    "theme",
+    "django_browser_reload",
+    
 ]
 
 MIDDLEWARE = [
