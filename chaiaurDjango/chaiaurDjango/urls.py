90fe038aa7153b1bf23231237b92d2746d2dd8a1
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-01-20 20:13:25 +0530

    complete Django basic working with task.✅

--- chaiaurDjango/chaiaurDjango/urls.py
+++ chaiaurDjango/chaiaurDjango/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
     path('about/', views.about,name='about'),
+    path('projects/', views.projects,name='projects'),
+    path('skills/', views.skills,name='skills'),
     path('contact/', views.contact,name='contact'),   
 ]
--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -2,10 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    # return HttpResponse("Hello World.")
+    # return HttpResponse("Hello , World from Himanshu.")
     return render(request,'website/index.html')
 
 def about(request):
-    return HttpResponse("Hello World. from About")
+    # return HttpResponse("Hello , World from Himanshu. About page")
+    return render(request,'website/about.html')
+
 def contact(request):
-    return HttpResponse("Hello World. from Contact.")
+    # return HttpResponse("Hello , World from Himanshu. Contact page")
+    return render(request,'website/contact.html')
+
+def skills(request):
+    # return HttpResponse("Hello , World from Himanshu. Skills page")
+    return render(request,'website/skills.html')
+
+def projects(request):
+    # return HttpResponse("Hello , World from Himanshu. projects page")
+    return render(request,'website/projects.html')
