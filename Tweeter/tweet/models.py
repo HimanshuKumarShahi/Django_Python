891e6d17de39374eb854cd676e8eb1f3335371f6
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-01-28 20:12:50 +0530

    correction

--- Tweeter/tweet/models.py
+++ Tweeter/tweet/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-# Create your models here.
+
 class Tweet(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     text=models.TextField(max_length=150)
