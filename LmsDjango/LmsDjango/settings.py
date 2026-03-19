4e46487de1a9588867eef3d64d7792e862578551
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2026-03-19 11:06:15 +0530

    Integrate django-browser-reload for live reloading, update URLs, and remove unused CSS file

--- LmsDjango/LmsDjango/settings.py
+++ LmsDjango/LmsDjango/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+     'django_browser_reload',
+
     'users',
     'tailwind',
     'theme',
@@ -51,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'LmsDjango.urls'
